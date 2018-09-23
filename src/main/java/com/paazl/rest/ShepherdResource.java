c05789c24beb9abfa6f29a64eb25f9112d1e44b6
GenaPC <usatenko.gena@gmail.com>
2018-09-23 12:09:33 +0300

    order new sheep

--- src/main/java/com/paazl/rest/ShepherdResource.java
+++ src/main/java/com/paazl/rest/ShepherdResource.java
@@ -1,11 +1,12 @@
 package com.paazl.rest;
 
+import com.paazl.service.SheepStatusesDto;
 import com.paazl.service.ShepherdService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import java.math.BigInteger;
 
 @Path("/shepherdmanager")
@@ -22,5 +23,23 @@ public class ShepherdResource {
     @Path("/balance")
     public BigInteger getBalance() {
         return service.getBalance();
+    }
+
+    @GET
+    @Path("/status")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getStatus() {
+        SheepStatusesDto sheepStatuses = service.getSheepStatusses();
+        return String.format("Balance: %d, number of sheep healthy and dead: [%d, %d]",
+                getBalance(),
+                sheepStatuses.getNumberOfHealthySheep(),
+                sheepStatuses.getNumberOfDeadSheep());
+    }
+
+    @POST
+    @Path("/order/{nofSheepDesired}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String orderNewSheep(@PathParam("nofSheepDesired") int nofSheepDesired) {
+        return service.orderNewSheep(nofSheepDesired);
     }
 }
--- src/main/java/com/paazl/service/ShepherdService.java
+++ src/main/java/com/paazl/service/ShepherdService.java
@@ -4,6 +4,7 @@ import com.paazl.data.Sheep;
 import com.paazl.data.State;
 import com.paazl.data.repositories.CurrentBalanceRepository;
 import com.paazl.data.repositories.SheepRepository;
+import com.paazl.util.CurrentBalanceUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -46,7 +47,20 @@ public class ShepherdService {
     public String orderNewSheep(int nofSheepDesired) {
         // TODO Implement sheep ordering feature
     	// TODO Write unit tests
+        int maxPossibleSheepNumber = getBalance().divide(BigInteger.valueOf(priceOfSheep)).intValue();
+        if(maxPossibleSheepNumber >= nofSheepDesired){
+            currentBalanceRepository.save(
+                    CurrentBalanceUtils.subtractBalance(
+                            currentBalanceRepository.findFirstByOrderByTimestampDesc(),
+                            BigInteger.valueOf(priceOfSheep).multiply(BigInteger.valueOf(nofSheepDesired))
+                    )
+            );
+            for (int i = 0; i < nofSheepDesired; ++i) {
+                sheepRepository.save(new Sheep());
+            }
+            return String.format("In total %s sheep were ordered and added to your flock!", nofSheepDesired);
+        }
+        return String.format("You don't have enough money to order %s sheep for your flock!", nofSheepDesired);
 
-        return String.format("In total %s sheep were ordered and added to your flock!", 333);
     }
 }
